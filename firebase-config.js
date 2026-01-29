bc4b19a8c3564fbbfd00f957cc4899a2800eed58
joyfulsprout <yang.geehyun@gmail.com>
2026-01-29 15:43:19 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-  apiKey: window.env.FIREBASE_API_KEY,
-  authDomain: window.env.FIREBASE_AUTH_DOMAIN,
-  projectId: window.env.FIREBASE_PROJECT_ID,
-  storageBucket: window.env.FIREBASE_PROJECT_ID + ".appspot.com",
-  messagingSenderId: "235936270485", 
-  appId: window.env.FIREBASE_APP_ID
+  apiKey: "FIREBASE_API_KEY_PLACEHOLDER",
+  authDomain: "FIREBASE_AUTH_DOMAIN_PLACEHOLDER",
+  projectId: "FIREBASE_PROJECT_ID_PLACEHOLDER",
+  storageBucket: "FIREBASE_PROJECT_ID_PLACEHOLDER.appspot.com",
+  messagingSenderId: "235936270485", // 본인 숫자 ID
+  appId: "FIREBASE_APP_ID_PLACEHOLDER"
 };
