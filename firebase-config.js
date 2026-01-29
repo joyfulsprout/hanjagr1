e6d814dbc236ae727f2a37e0a2d0bd9861c4af58
joyfulsprout <yang.geehyun@gmail.com>
2026-01-29 15:51:49 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
-const firebaseConfig = {
+var firebaseConfig = {
   apiKey: "FIREBASE_API_KEY_PLACEHOLDER",
   authDomain: "FIREBASE_AUTH_DOMAIN_PLACEHOLDER",
   projectId: "FIREBASE_PROJECT_ID_PLACEHOLDER",
   storageBucket: "FIREBASE_PROJECT_ID_PLACEHOLDER.appspot.com",
-  messagingSenderId: "235936270485", // 본인 숫자 ID
+  messagingSenderId: "235936270485", // 본인의 실제 Messaging Sender ID 숫자를 넣으셔도 됩니다.
   appId: "FIREBASE_APP_ID_PLACEHOLDER"
 };
