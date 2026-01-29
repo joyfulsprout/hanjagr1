e5418be0cad26d77c33fe2b2d86916a39919b0eb
joyfulsprout <yang.geehyun@gmail.com>
2026-01-29 15:19:04 +0900

    Add files via upload
    first upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "FIREBASE_API_KEY_PLACEHOLDER",
+  authDomain: "FIREBASE_AUTH_DOMAIN_PLACEHOLDER",
+  projectId: "FIREBASE_PROJECT_ID_PLACEHOLDER",
+  storageBucket: "FIREBASE_PROJECT_ID_PLACEHOLDER.appspot.com",
+  messagingSenderId: "235936270485", 
+  appId: "FIREBASE_APP_ID_PLACEHOLDER"
+};
