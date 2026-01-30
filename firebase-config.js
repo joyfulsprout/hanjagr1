6d5708791a2e41448cb19b09113728479e81810f
joyfulsprout <yang.geehyun@gmail.com>
2026-01-30 05:24:35 +0000

    Deploying to gh-pages from @ joyfulsprout/hanjagr1@e5422add36338669b87dc382ed541a434221698d 🚀

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 var firebaseConfig = {
-  apiKey: 'AIzaSyDoFSK1EtcRDaQI5JU4m0NR5fcDj2bJW1Y
+  apiKey: "AIzaSyDoFSK1EtcRDaQI5JU4m0NR5fcDj2bJW1Y
 
-',
-  authDomain: 'hanja-gr1.firebaseapp.com',
-  projectId: 'hanja-gr1',
-  storageBucket: 'hanja-gr1.appspot.com',
-  messagingSenderId: '235936270485',
-  appId: 'G-HR4BYJ7N9Q'
+",
+  authDomain: "hanja-gr1.firebaseapp.com",
+  projectId: "hanja-gr1",
+  storageBucket: "hanja-gr1.appspot.com",
+  messagingSenderId: "235936270485",
+  appId: "G-HR4BYJ7N9Q"
 };
